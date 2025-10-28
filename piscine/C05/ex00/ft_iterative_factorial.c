82396b1d13122f3b8685f8093096f9d53f1de7ab
Zeyd Maache <zmaache@c2r9s5.42luxembourg.lu>
2025-10-28 17:30:04 +0100

    gitlx

---
+++ piscine/C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zmaache <zmaache@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 15:28:41 by zmaache           #+#    #+#             */
+/*   Updated: 2025/10/28 16:07:27 by zmaache          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	res;
+
+	res = 1;
+	if (nb < 0)
+	return (0);
+	while (nb > 1)
+	{
+		res *= nb;
+		nb--;
+	}
+	return (res);
+}
+
+/* int	main(void)
+{
+	int	nb;
+
+	nb = 10;
+	ft_iterative_factorial(nb);
+} */
